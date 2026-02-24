fa2042a8e811faa55fd31f2e9e822ac49c067ce7
mtparceiros-alt <mtparceiros@gmail.com>
2026-02-24 19:55:04 -0300

    Delete _check_coords.py

--- _check_coords.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('empreendimentos.json', encoding='utf-8') as f:
-    data = json.load(f)
-for e in data:
-    print(f"{e['nome'][:40]:<40} | {e['endereco'][:40]:<40} | {e['lat']:.5f}, {e['lng']:.5f}")
